2f4f7af9279799fb16a82b5fad2cc846368056f6
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-16 11:47:12 +0300

    Initial project.

---
+++ src/main/java/com/pvpgame/PvpGameApplication.java
@@ -0,0 +1,13 @@
+package com.pvpgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvpGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvpGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvpgame/PvpGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvpgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvpGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
