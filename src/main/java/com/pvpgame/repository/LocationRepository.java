e10001bfaf69e85a95aa4fe4969f3c8bc7c0dd3c
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-16 20:18:12 +0300

    Create methods for get enemies and players by location id.

--- src/main/java/com/pvpgame/repository/LocationRepository.java
+++ src/main/java/com/pvpgame/repository/LocationRepository.java
@@ -1,9 +1,20 @@
 package com.pvpgame.repository;
 
+import com.pvpgame.model.Enemy;
 import com.pvpgame.model.Location;
+import com.pvpgame.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Query("SELECT p FROM Player p WHERE p.location.id = :locationId")
+    List<Player> findPlayersByLocationId(@Param("locationId") Long locationId);
+
+    @Query("SELECT e FROM Enemy e WHERE e.location.id = :locationId")
+    List<Enemy> findEnemiesByLocationId(@Param("locationId") Long locationId);
 }
