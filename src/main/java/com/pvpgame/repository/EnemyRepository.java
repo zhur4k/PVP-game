bf5ddb787c212c7cf2ff0e8aab6d01da197bb941
zhur4k <kostya.zhurovich12052@gmail.com>
2024-10-16 15:53:22 +0300

    Create repositories.

---
+++ src/main/java/com/pvpgame/repository/EnemyRepository.java
@@ -0,0 +1,9 @@
+package com.pvpgame.repository;
+
+import com.pvpgame.model.Enemy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnemyRepository extends JpaRepository<Enemy, Long> {
+}
